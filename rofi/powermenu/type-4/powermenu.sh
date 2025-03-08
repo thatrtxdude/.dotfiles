08dc54ab68df7e8a12eee8387ee34b7059d074dc
thatrtxdude <phillip.oertel.hamm@gmail.com>
2025-03-08 19:39:54 +0100

    thinkpad branch

--- rofi/powermenu/type-4/powermenu.sh
+++ rofi/powermenu/type-4/powermenu.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 ## Author : Aditya Shakya (adi1090x)
 ## Github : @adi1090x
 #
@@ -8,24 +7,20 @@
 ## Available Styles
 #
 ## style-1   style-2   style-3   style-4   style-5
-
 # Current Theme
 dir="$HOME/.config/rofi/powermenu/type-4"
-theme='style-5'
-
+theme='style-3'
 # CMDs
 uptime="`uptime -p | sed -e 's/up //g'`"
 host=`hostname`
-
 # Options
-shutdown=''
-reboot=''
-lock=''
-suspend=''
-logout=''
-yes=''
-no=''
-
+shutdown='⏻'
+reboot=''
+lock='󰌾'
+suspend='󰤄'
+logout='󰍃'
+yes='yes'
+no='no'
 # Rofi CMD
 rofi_cmd() {
 	rofi -dmenu \
@@ -33,7 +28,6 @@ rofi_cmd() {
 		-mesg "Uptime: $uptime" \
 		-theme ${dir}/${theme}.rasi
 }
-
 # Confirmation CMD
 confirm_cmd() {
 	rofi -dmenu \
@@ -41,29 +35,47 @@ confirm_cmd() {
 		-mesg 'Are you Sure?' \
 		-theme ${dir}/shared/confirm.rasi
 }
-
 # Ask for confirmation
 confirm_exit() {
 	echo -e "$yes\n$no" | confirm_cmd
 }
-
 # Pass variables to rofi dmenu
 run_rofi() {
 	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
-
 # Execute Command
 run_cmd() {
 	selected="$(confirm_exit)"
 	if [[ "$selected" == "$yes" ]]; then
 		if [[ $1 == '--shutdown' ]]; then
-			systemctl poweroff
+			# Check for Void Linux (runit) or fallback to systemd
+			if command -v loginctl &> /dev/null; then
+				loginctl poweroff
+			elif [ -d "/etc/runit" ]; then
+				sudo shutdown -p now
+			else
+				systemctl poweroff
+			fi
 		elif [[ $1 == '--reboot' ]]; then
-			systemctl reboot
+			# Check for Void Linux (runit) or fallback to systemd
+			if command -v loginctl &> /dev/null; then
+				loginctl reboot
+			elif [ -d "/etc/runit" ]; then
+				sudo reboot
+			else
+				systemctl reboot
+			fi
 		elif [[ $1 == '--suspend' ]]; then
 			mpc -q pause
 			amixer set Master mute
-			systemctl suspend
+			# Check for Void Linux (runit) or fallback to systemd
+			if command -v loginctl &> /dev/null; then
+				loginctl suspend
+			elif [ -d "/etc/runit" ]; then
+				sudo zzz
+			else
+				systemctl suspend
+			fi
 		elif [[ $1 == '--logout' ]]; then
 			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
 				openbox --exit
@@ -73,13 +85,17 @@ run_cmd() {
 				i3-msg exit
 			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
 				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+			elif [[ "$XDG_CURRENT_DESKTOP" == 'Hyprland' ]]; then
+				hyprctl dispatch exit
+			elif [[ -n "$HYPRLAND_INSTANCE_SIGNATURE" ]]; then
+				# Alternative detection for Hyprland
+				hyprctl dispatch exit
 			fi
 		fi
 	else
 		exit 0
 	fi
 }
-
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
@@ -94,6 +110,10 @@ case ${chosen} in
 			betterlockscreen -l
 		elif [[ -x '/usr/bin/i3lock' ]]; then
 			i3lock
+		elif [[ -x '/usr/bin/swaylock' ]]; then
+			swaylock
+		elif [[ -x '/usr/bin/hyprlock' ]]; then
+			hyprlock
 		fi
         ;;
     $suspend)
--- waybar/scripts/mediaplayer.sh
+++ waybar/scripts/mediaplayer.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 player_status=$(playerctl status 2> /dev/null)
 artist=$(playerctl metadata artist 2> /dev/null)
